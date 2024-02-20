fccd7d5de342f4e181a89a69e272a5039ddd717d
Davi <davi.campos689@gmail.com>
2024-02-20 16:14:56 -0300

    feat: implement register in page

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -7,17 +7,70 @@ import {
   Center,
   Flex,
 } from "@mantine/core";
+import { notifications } from "@mantine/notifications";
 import Link from "next/link";
-import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../providers/authContext";
+import { BadRequestError } from "../providers/errors/bad-request.error";
 
 export default function RegisterPage() {
-  const [fullName, setFullName] = useState("");
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
 
-  const handleLogin = (e: any) => {
+  const { register } = useContext(AuthContext);
+  const router = useRouter();
+
+  const handleLogin = async (e: any) => {
     e.preventDefault();
+    try {
+      if (password !== confirmPassword) {
+        notifications.show({
+          title: "Passwords do not match",
+          message: "Try again",
+          color: "red",
+          position: "top-right",
+          autoClose: 3000,
+        });
+
+        return;
+      }
+
+      await register(name, email, password);
+
+
+      notifications.show({
+        title: 'Success',
+        message: 'User registered with success',
+        color: "green",
+        position: "top-right",
+        autoClose: 3000,
+      });
+
+      router.push('/login')
+
+
+    } catch (error) {
+      console.log("error section");
+      console.log(error);
+      let title = "Ops";
+      let message = "Some Server Error, Try Again!";
+
+      if (error instanceof BadRequestError) {
+        title = error.message;
+        message = "Verify your info";
+      }
+
+      notifications.show({
+        title,
+        message: message,
+        color: "red",
+        position: "top-right",
+        autoClose: 3000,
+      });
+    }
   };
 
   return (
@@ -32,14 +85,16 @@ export default function RegisterPage() {
         </Center>
         <form onSubmit={handleLogin}>
           <TextInput
+          name="name"
             label="Full Name"
             placeholder="John Doe"
-            value={fullName}
-            onChange={(e) => setFullName(e.target.value)}
+            value={name}
+            onChange={(e) => setName(e.target.value)}
             style={{ marginBottom: 15 }}
             required
           />
           <TextInput
+          name="email"
             type="email"
             label="Email"
             placeholder="user@mail.com"
@@ -49,6 +104,7 @@ export default function RegisterPage() {
             required
           />
           <TextInput
+          name="password"
             type="password"
             label="Password"
             placeholder="Password"
@@ -58,6 +114,7 @@ export default function RegisterPage() {
             required
           />
           <TextInput
+          name="confirmPassword"
             type="password"
             label="Confirm Password"
             placeholder="Confirm Password"
