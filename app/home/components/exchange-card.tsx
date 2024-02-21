e47d30b146b979cf63372fd7f32b77aacffe0880
Davi <davi.campos689@gmail.com>
2024-02-21 01:44:03 -0300

    refactor: rename component

--- app/home/components/exchange-card.tsx
+++ app/home/components/exchange-card.tsx
@@ -1,36 +1,33 @@
 "use client";
 import { Card, Image, Text, Badge, Button, Group, Paper } from "@mantine/core";
 
-const getImageUrl = (i: string): string => `card-img/${i}.png`
+const getImageUrl = (i: string): string => `card-img/${i}.png`;
 
 interface CardExchangeInfoProps {
-  code_source: string
-  code_destination: string
-  acronym: string
+  code_source: string;
+  code_destination: string;
+  acronym: string;
   max: number;
   min: number;
   avg: number;
 }
 
-export default function CardExchangeInfo(input: CardExchangeInfoProps) {
+export default function ExchangeCard(input: CardExchangeInfoProps) {
   return (
     <Paper p="md" style={{ maxWidth: 400, margin: "auto" }}>
       <Card padding="lg" radius="md" withBorder>
         <Card.Section>
-          <Image
-            src={getImageUrl(input.acronym)}
-            height={160}
-            alt="Norway"
-          />
+          <Image src={getImageUrl(input.acronym)} height={160} alt="Norway" />
         </Card.Section>
 
-        <Group  justify="space-between" mt="md" mb="xs" >
-          <Text ta="start" fw={500}>{`${input.code_source} => ${input.code_destination}`}</Text>
-          {/* <Badge color="green">Favorite</Badge> */}
+        <Group justify="space-between" mt="md" mb="xs">
+          <Text
+            ta="start"
+            fw={500}
+          >{`${input.code_source} => ${input.code_destination}`}</Text>
         </Group>
 
-        {/* Table-like section */}
-        <div  style={{ marginTop: "20px" , textAlign:'center' }}>
+        <div style={{ marginTop: "20px", textAlign: "center" }}>
           <div
             style={{
               display: "flex",
